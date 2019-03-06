54f25bc8fc3fcf6bcf647ac3136a7a2abc8415e9
Luc <lucdenridder@hotmail.nl>
2019-03-06 12:17:00 +0100

    Matrix

--- parameters.m
+++ parameters.m
@@ -111,10 +111,13 @@ Cnda   =  -0.0120;
 Cndr   =  -0.0939;
 
 syms u alpha theta q udot alphadot thetadot qdot de
+C = eye(4);
+D = 0;
 %symmetric
-state_vector_symmetric = [u; alpha; theta; q];
-state_vector_dot_symmetric = [udot; alphadot; thetadot; qdot];
-input_vector_symmetric = [de];
+% state_vector_symmetric = [u; alpha; theta; q];
+% state_vector_dot_symmetric = [udot; alphadot; thetadot; qdot];
+% input_vector_symmetric = [de];
+
 C1_symmetric = [-2*muc*c/V0^2, 0, 0, 0; 0, (CZadot - 2*muc)*c/V0, 0, 0; 0, 0, -c/V0, 0; 0, Cmadot*c/V0, 0, -2*muc*KY2*(c/V0)^2];
 C2_symmetric = [CXu/V0, CXa, CZ0, CXq*c/V0; CZu/V0, CZa, -CX0, (CZq+2*muc)*c/V0; 0, 0, 0, c/V0; Cmu/V0 Cma 0 Cmq*c/V0];
 C3_symmetric = [CXde; CZde;0;Cmde];
@@ -124,8 +127,14 @@ A2 = [V0*CXu/(2*muc*c), V0^2*CXa/(2*muc*c), V0^2*CZ0/(2*muc*c), V0*CXq/(2*muc); 
 B2 = [V0^2*CXde/(2*muc*c);-V0^2*CZde/((CZadot-2*muc)*c);0;V0^2/(2*muc*KY2*c^2)*(Cmde-(Cmadot*CZde)/(CZadot-2*muc))];
 
 %asymmetric
-C1_asymmetric = [(CYbdot-2*mub)*b/V0, 0, 0 ,0; 0, -b/(2*V0), 0, 0; 0, 0, -2*mub*KX2*b^2/V0^2, 2*mub*KXZ*b^2/V0^2; Cnb*b/V0, 0, 2*mub*KXZ*b^2/V0^2, -2*mub*KZ2*b^2/V0^2]
+C1_asymmetric = [(CYbdot-2*mub)*b/V0, 0, 0 ,0; 0, -b/(2*V0), 0, 0; 0, 0, -2*mub*KX2*b^2/V0^2, 2*mub*KXZ*b^2/V0^2; Cnb*b/V0, 0, 2*mub*KXZ*b^2/V0^2, -2*mub*KZ2*b^2/V0^2];
+C2_asymmetric = [CYb, CL, CYp*b/(2*V0), (CYr-4*mub)*b/(2*V0);0, 0, b/(2*V0), 0; Clb, 0, Clp*b/(2*V0), Clr*b/(2*V0); Cnb, 0 , Cnp*b/(2*V0), Cnr*b/(2*V0)];
+C3_asymmetric = [CYda,CYdr; 0, 0; Clda, Cldr; Cnda, Cndr];
+A1_asymmetric = -inv(C1_asymmetric)*C2_asymmetric;
+B1_asymmetric = -inv(C1_asymmetric)*C3_asymmetric;
 
+sys = ss(A1_symmetric,B1_symmetric,C,D)
+sys = ss(A1_asymmetric,B1_asymmetric,C,D)
 
 % symmetric_eq1 = [CXu-2*muc*c/V0*(ddt), CXa, CZ0, CXq; CZu, CZa+(CZadot-2*muc)*c/V0*(ddt), -CX0, CZq+2*muc; 0, 0, -c/V0*(ddt), 1; Cmu, Cma+Cmadot*c/V0*(ddt),0,Cmq-2*muc*KY2]
 % symmetric_eq2 = [ucirc;alpha;theta;q*c/V0]
