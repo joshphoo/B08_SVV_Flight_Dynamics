3af0715e17783c3166c5fe04f7850359a03f915f
willem <willemvl@live.com>
2019-03-20 11:04:26 +0100

    Main wat uitgebreid. Werkt alleen als de "run" parts in A_C worden ge%

--- main.m
+++ main.m
@@ -9,7 +9,11 @@ run('excell_data_reader_ref.m')
 run('excell_data_reader_real.m')
 %----------------------Obtain parameters from Marloes----------------------
 run('parametersmarloes.m')
-%----------------------Run Engine Part-------------------------------------
+%----------------------Obtain the Mass Balance-----------------------------
+run('MassBalance.m')
+%----------------------Obtain Engine Data-------------------------------------
 run('Engine_part.m')
+%----------------------Obtain Aerodynamic coefficients---------------------
+run('Aerodynamic_Coefficients.m')
 %----------------------Run Flightdata Graphmaker---------------------------
 run('flightdata_graphmaker.m')
